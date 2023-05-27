f04543a215be315ef19b97ac5c4a0e0a1696c953
deptno <deptno@gmail.com>
2023-05-28 01:36:54 +0900

    apps/wiki: try-catch 추가

--- apps/wiki/app/[md]/page.tsx
+++ apps/wiki/app/[md]/page.tsx
@@ -4,12 +4,17 @@ import fs from 'node:fs/promises'
 import { parse } from 'parser-vimwiki'
 
 export default async (props: Props) => {
-  const markdown = await fs.readFile(`./${props.params.md}.md`)
-    .then((buffer) => buffer.toString())
-    .then(parse)
-  const html = marked(markdown)
+  try {
+    const markdown = await fs.readFile(`/mnt/data/${props.params.md ?? 'index'}.md`)
+      .then((buffer) => buffer.toString())
+      .then(parse)
+    const html = marked(markdown)
 
-  return <pre dangerouslySetInnerHTML={{ __html: html }}/>
+    return <pre dangerouslySetInnerHTML={{ __html: html }}/>
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
 }
 
 type Props = {
