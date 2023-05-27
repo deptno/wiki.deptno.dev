8c01ad81e0cadd65b56dffe6079b429ffe7ba4f6
deptno <deptno@gmail.com>
2023-05-28 01:37:06 +0900

    apps/wiki: 기본페이지를 index.md 로 매치

--- apps/wiki/app/page.tsx
+++ apps/wiki/app/page.tsx
@@ -1,10 +1,26 @@
-import { Button, Header } from "ui";
+import React from 'react'
+import { marked } from 'marked'
+import fs from 'node:fs/promises'
+import { parse } from 'parser-vimwiki'
 
-export default function Page() {
-  return (
-    <>
-      <Header text="Docs" />
-      <Button />
-    </>
-  );
+export default async (props: Props) => {
+  console.debug({ props })
+
+  try {
+    const markdown = await fs.readFile(`/mnt/data/index.md`)
+      .then((buffer) => buffer.toString())
+      .then(parse)
+    const html = marked(markdown)
+
+    return <pre dangerouslySetInnerHTML={{ __html: html }}/>
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+}
+
+type Props = {
+  params: {
+    md: string
+  }
 }
