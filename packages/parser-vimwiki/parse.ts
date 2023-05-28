46cf96fdad3c9950001ec2bd6135021b33811e99
deptno <deptno@gmail.com>
2023-05-28 21:35:43 +0900

    parser-vimwiki: regexp 를 상수로 빼서 사용

--- packages/parser-vimwiki/parse.ts
+++ packages/parser-vimwiki/parse.ts
@@ -1,5 +1,6 @@
 import { link } from './link'
+import { RE_VIMWIKI_LINK } from './constant'
 
 export function parse(markdown: string) {
-  return markdown.replace(/\[\[(.+)\]\]/g, link)
+  return markdown.replace(RE_VIMWIKI_LINK, link)
 }
