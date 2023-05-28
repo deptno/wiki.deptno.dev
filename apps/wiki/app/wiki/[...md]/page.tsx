8430135a09bf745d9b252603e66e20d86de328e7
deptno <deptno@gmail.com>
2023-05-29 03:00:46 +0900

    디렉토리로 링크가 들어온경우 index 리다이렉트 및 무한 루프 방지

--- apps/wiki/app/wiki/[...md]/page.tsx
+++ apps/wiki/app/wiki/[...md]/page.tsx
@@ -4,6 +4,7 @@ import { createRelativeLinkReplacer, parse } from 'parser-vimwiki'
 import { DIR_WIKI } from '../../../constant'
 import { marked } from '../../../lib/marked'
 import { Breadcrumbs } from '../../../component/Breadcrumbs'
+import { redirect } from 'next/navigation'
 
 export default async (props: Props) => {
   const { md } = props.params
@@ -30,9 +31,10 @@ export default async (props: Props) => {
       </>
     )
   } catch (err) {
-    console.error(err)
-
-    throw err
+    if (path.endsWith('index')) {
+      throw new Error(`unknown path: ${path}`)
+    }
+    return redirect(decodeURIComponent(`/wiki/${path}/index`))
   }
 }
 
