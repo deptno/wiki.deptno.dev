46de365f3c50c551008471df7594e4e08c051911
deptno <deptno@gmail.com>
2023-05-29 21:21:43 +0900

    wiki: 추적 스크립트들 삽입

--- apps/wiki/app/layout.tsx
+++ apps/wiki/app/layout.tsx
@@ -1,11 +1,44 @@
-export default function RootLayout({
-  children,
-}: {
+import React from 'react'
+import Script from 'next/script'
+import { GOOGLE_ANALYTICS_ID, MS_CLARITY_ID } from '../constant'
+
+export default function RootLayout({ children }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
+      <Script
+        async
+        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        crossOrigin="anonymous"
+      />
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+        async
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${GOOGLE_ANALYTICS_ID}');`,
+        }}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "${MS_CLARITY_ID}");
+  `,
+        }}
+      />
       <body>{children}</body>
     </html>
-  );
+  )
 }
