312e10611d7d934eca6190fb5eb5bb6372c36a3a
deptno <deptno@gmail.com>
2023-05-29 23:57:22 +0900

    markdown-ui: wip Markdown 으로 모듈화, 가로스크롤 내부에서만 되도록 변경

---
+++ apps/wiki/component/Markdown.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+
+export const Markdown: FC<Props> = (props) => {
+  return (
+    <div className="w-full overflow-x-hidden">
+      <pre
+        className="bg-gray-100 markdown p-8 whitespace-pre-wrap"
+        dangerouslySetInnerHTML={{ __html: props.data }}
+      />
+    </div>
+  )
+}
+
+type Props = {
+  data: string
+}
