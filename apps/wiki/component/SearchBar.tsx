14bb8c0175118ac3a1d57631bc4700865677576b
deptno <deptno@gmail.com>
2023-05-30 01:19:50 +0900

    markdown-ui: 검색바 개선
    - UI 개선
    - 서버에서는 인스턴스 서치 인스턴스화를 하지 않도록 함
    - 검색어에 대해서 키값을 통해 페이지로 이동할 수 있도록 함
      - [ ] TODO: 중첩된 디렉토리내의 페이지도 이동할 수 있도록 키 값에
        디렉토리가 포함되어야함

--- apps/wiki/component/SearchBar.tsx
+++ apps/wiki/component/SearchBar.tsx
@@ -1,39 +1,61 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch-hooks-web'
-import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
-import { marked } from '../lib/marked'
+import { instantMeiliSearch, InstantMeiliSearchInstance } from '@meilisearch/instant-meilisearch'
+import Link from 'next/link'
 
-const searchClient = instantMeiliSearch(
-  process.env.NEXT_PUBLIC_MEILISEARCH_HOST,
-  undefined,
-  {
-    placeholderSearch: false,
-    finitePagination: true,
-    primaryKey: 'id',
-    requestConfig: {},
-  },
-)
 
-export const SearchBar = () => (
-  <InstantSearch
-    indexName="wiki"
-    searchClient={searchClient}
-  >
-    <SearchBox/>
-    <Hits hitComponent={Hit}/>
-  </InstantSearch>
-)
-const Hit = (props) => {
-  const { hit } = props
+export const SearchBar = () => {
+  const [searchClient, setSearchClient] = useState<InstantMeiliSearchInstance>()
+
+  useEffect(() => {
+    const searchClient = instantMeiliSearch(
+      process.env.NEXT_PUBLIC_MEILISEARCH_HOST,
+      undefined,
+      {
+        placeholderSearch: false,
+        finitePagination: true,
+        primaryKey: 'id',
+        requestConfig: {},
+      },
+    )
+
+    setSearchClient(searchClient)
+  }, [])
+
+  if (!searchClient) {
+    return <div>로딩...</div>
+  }
 
   return (
-    <div>
-      <span>
-      {Buffer.from(hit.id, 'hex').toString()}
-      </span>
-      <Highlight attribute="content" hit={hit}/>
-      <pre dangerouslySetInnerHTML={{ __html: marked(hit.content) }}/>
+    <InstantSearch
+      indexName="wiki"
+      searchClient={searchClient}
+    >
+      <div className="flex flex-col">
+        <SearchBox/>
+        <div
+          className="absolute top-7 left-0 bg-stone-400 w-full whitespace-pre-wrap break-words break-all text-xs overflow-x-hidden">
+          <div className="overflow-x-scroll">
+            <Hits hitComponent={Hit}/>
+          </div>
+        </div>
+      </div>
+    </InstantSearch>
+  )
+}
+const Hit = (props) => {
+  const { hit } = props
+  const id = Buffer.from(hit.id, 'hex').toString()
+
+  return (
+    <div className="grid grid-cols-[100px_1fr] border-b-2">
+      <Link className="font-bold underline text-blue-800" href={`/wiki/${id}`}>
+        {id}
+      </Link>
+      <div className="flex-1">
+        <Highlight attribute="content" hit={hit}/>
+      </div>
     </div>
   )
 }
