19e96643574b41a04e62fce6912a9ad9b4fb854d
deptno <deptno@gmail.com>
2023-05-30 01:19:05 +0900

    markdown-ui: padding 재정의

--- apps/wiki/component/Markdown.tsx
+++ apps/wiki/component/Markdown.tsx
@@ -4,7 +4,7 @@ export const Markdown: FC<Props> = (props) => {
   return (
     <div className="w-full overflow-x-hidden">
       <pre
-        className="bg-gray-100 markdown p-8 whitespace-pre-wrap"
+        className="bg-gray-100 markdown p-2 md:p-4 lg:p-8 whitespace-pre-wrap"
         dangerouslySetInnerHTML={{ __html: props.data }}
       />
     </div>
