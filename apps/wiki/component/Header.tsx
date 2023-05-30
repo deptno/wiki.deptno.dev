3dd2996303a52180165c9bc8ed33e5353767a934
deptno <deptno@gmail.com>
2023-05-31 02:50:40 +0900

    헤더 색상 변경

--- apps/wiki/component/Header.tsx
+++ apps/wiki/component/Header.tsx
@@ -4,7 +4,7 @@ import { Breadcrumbs } from './Breadcrumbs'
 
 export const Header: FC<Props> = (props) => {
   return (
-    <header className="flex justify-between pl-2">
+    <header className="flex justify-between p-1 bg-gray-800 text-white">
       <Breadcrumbs/>
       <SearchBar/>
     </header>
