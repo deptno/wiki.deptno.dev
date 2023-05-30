866923b538eb7ab742978c156e89c1786496da18
deptno <deptno@gmail.com>
2023-05-30 22:39:58 +0900

    wiki: toc 를 위한 공간 확보 및 전체 백그라운드 색 통일

--- apps/wiki/app/layout.tsx
+++ apps/wiki/app/layout.tsx
@@ -1,4 +1,9 @@
 import './global.css'
+import './sm.css'
+import './md.css'
+import './lg.css'
+import './xl.css'
+import './2xl.css'
 import React from 'react'
 import Script from 'next/script'
 import { GOOGLE_ANALYTICS_ID, MS_CLARITY_ID } from '../constant'
@@ -8,38 +13,46 @@ export default function RootLayout({ children }: {
 }) {
   return (
     <html lang="ko">
-      <Script
-        async
-        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        crossOrigin="anonymous"
-      />
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-        async
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
+    <Script
+      async
+      src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      crossOrigin="anonymous"
+    />
+    <Script
+      strategy="afterInteractive"
+      src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+      async
+    />
+    <Script
+      id="gtag-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', '${GOOGLE_ANALYTICS_ID}');`,
-        }}
-      />
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(c,l,a,r,i,t,y){
+      }}
+    />
+    <Script
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(c,l,a,r,i,t,y){
   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
   })(window, document, "clarity", "script", "${MS_CLARITY_ID}");
   `,
-        }}
-      />
-      <body className="text-xs md:text-lg lg:text-xl m-0">{children}</body>
+      }}
+    />
+    <body className="text-xs md:text-lg md:flex justify-center m-0 md:min-w-md bg-gray-100">
+      <main className="w-full max-w-screen-md md:border border-gray-800 min-h-screen">
+        {children}
+      </main>
+      <aside className="main fixed hidden 2xl:block w-96 p-2 break-words">
+        sidebar
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dicta, earum excepturi iure maiores quibusdam repellat vitae. Accusamus dolorem earum eos, id minus mollitia nisi omnis repellendus sed vel voluptates?
+      </aside>
+    </body>
     </html>
   )
 }
--- apps/wiki/component/Markdown.tsx
+++ apps/wiki/component/Markdown.tsx
@@ -3,8 +3,13 @@ import React, { FC } from 'react'
 export const Markdown: FC<Props> = (props) => {
   return (
     <div className="w-full overflow-x-hidden">
+      <aside className="2xl:hidden bg-gray-100 p-2">
+        <div className="border border-gray-800 p-2 break-words">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda dicta, earum excepturi iure maiores quibusdam repellat vitae. Accusamus dolorem earum eos, id minus mollitia nisi omnis repellendus sed vel voluptates?
+        </div>
+      </aside>
       <pre
-        className="bg-gray-100 markdown p-2 md:p-4 lg:p-8 whitespace-pre-wrap"
+        className="bg-gray-100 markdown p-2 whitespace-pre-wrap"
         dangerouslySetInnerHTML={{ __html: props.data }}
       />
     </div>
