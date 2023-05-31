b050f9795db15b3e109c06d1e442358fcff85b8c
deptno <deptno@gmail.com>
2023-06-01 00:55:52 +0900

    wiki: wip seo

--- apps/wiki/app/wiki/[...md]/page.tsx
+++ apps/wiki/app/wiki/[...md]/page.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React, { cache } from 'react'
 import fs from 'node:fs/promises'
 import { createRelativeLinkReplacer, parse } from 'parser-vimwiki'
-import { DIR_WIKI } from '../../../constant'
+import { DIR_WIKI, RE_RELATION } from '../../../constant'
 import { marked } from '../../../lib/marked'
 import { redirect } from 'next/navigation'
 import { Header } from '../../../component/Header'
 import { NoPage } from '../../../component/NoPage'
 import { Markdown } from '../../../component/Markdown'
+import { channel } from 'diagnostics_channel'
 
 export default async (props: Props) => {
   const { md } = props.params
   const nested = Array.isArray(md)
+  console.log({nested})
   const path = nested
     ? md.join('/')
     : md
   const currentPath = nested
     ? md.slice(0, -1).join('/')
     : undefined
-  const file = decodeURIComponent(`${DIR_WIKI}/${path}.md`)
 
   try {
-    const markdown = await fs.readFile(file)
-      .then((buffer) => buffer.toString())
+    const markdown = await getData(path)
       .then(createRelativeLinkReplacer(currentPath))
       .then(parse)
     const html = marked(markdown)
@@ -40,9 +40,58 @@ export default async (props: Props) => {
     return <NoPage name={path}/>
   }
 }
-
 type Props = {
   params: {
-    md: string | string[]
+    md: string[]
   }
 }
+
+export async function generateMetadata({ params }: Props) {
+  const path = params.md.join('/')
+  const markdown = await getData(path)
+  const [title, ...lines] = markdown.split('\n')
+  const index = lines.findLastIndex((l) => l.includes('# related'))
+  console.log({index})
+  const keywords = index !== -1
+    ? lines
+      .slice(index + 1)
+      .map((l) => {
+        return RE_RELATION.exec(l)?.[1]
+      })
+      .filter(Boolean)
+      .slice(0, 12)
+    : []
+  const description = lines.join('\n').slice(0, 150)
+  console.log({
+    path,
+        title,
+        description,
+        keywords,
+        openGraph: {
+          title,
+          description,
+          locale: 'ko',
+          siteName: 'https://deptno.dev',
+          url: 'https://deptno.dev/wiki/' + path,
+        },
+      })
+
+  return {
+    title,
+    description,
+    keywords,
+    openGraph: {
+      title,
+      description,
+      locale: 'ko',
+      siteName: 'https://deptno.dev',
+      url: 'https://deptno.dev/wiki/' + path,
+    },
+  }
+}
+
+const getData = cache(async (path: string) => {
+  const file = decodeURIComponent(`${DIR_WIKI}/${path}.md`)
+
+  return fs.readFile(file).then((buffer) => buffer.toString())
+})
