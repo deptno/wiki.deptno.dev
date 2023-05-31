5b0d07baf3e00bf516990a8fad85f12988a26a6c
deptno <deptno@gmail.com>
2023-05-31 22:59:06 +0900

    wiki: markdown 에 TOC 적용

--- apps/wiki/component/Markdown.tsx
+++ apps/wiki/component/Markdown.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { URL_WIKI } from '../constant'
+import { TOC } from '../lib/TOC'
 
 export const Markdown: FC<Props> = (props) => {
   const { data, path = '' } = props
@@ -7,11 +8,12 @@ export const Markdown: FC<Props> = (props) => {
   return (
     <div className="w-full overflow-x-hidden">
       <div className="p-2">
-        <aside id="sidebar" className="px-2 break-words border border-gray-800">
-          {!path.includes('/') && <div className="flex gap-2">
+        <aside id="sidebar" className="px-2 break-words border border-gray-800 3xl:text-md 3xl:border-hidden">
+          {!path.includes('/') && <div className="flex justify-end gap-2 border-b-2">
             <a className="underline border-l-blue-400" href={`${URL_WIKI}/${path}/_edit`} target="_blank">수정</a>
             <a className="underline border-l-blue-800" href={`${URL_WIKI}/${path}/_history`} target="_blank">기록</a>
           </div>}
+          <TOC html={data}/>
         </aside>
       </div>
       <pre
