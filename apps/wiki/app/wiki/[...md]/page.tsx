2f56f4992602570b5d813e412f7ec97c816600f0
deptno <deptno@gmail.com>
2023-06-01 16:50:09 +0900

    wiki: generateMetadata 에서 현재 제공 가능 한 것만 제공
    - [ ] TODO: 문서 컨벤션으로 SEO 제공하자, 상세 내용 확인
      - title `# kubernetes|쿠버네티스` 형태라면 | 이후가 제목이 되어야할 것
      - description 타이틀 라인 이후에 나오는 `>` 블락을 컨벤션으로 가져가는게 좋을 것 같음
      - keyword related 혹은 내부 링크 문서를 가지고 키워드화

--- apps/wiki/app/wiki/[...md]/page.tsx
+++ apps/wiki/app/wiki/[...md]/page.tsx
@@ -1,29 +1,23 @@
-import React, { cache } from 'react'
+import React from 'react'
 import fs from 'node:fs/promises'
-import { createRelativeLinkReplacer, parse } from 'parser-vimwiki'
-import { DIR_WIKI, RE_RELATION } from '../../../constant'
+import { createRelativeLinkReplacer } from 'parser-vimwiki'
+import { DIR_WIKI } from '../../../constant'
 import { marked } from '../../../lib/marked'
 import { redirect } from 'next/navigation'
 import { Header } from '../../../component/Header'
 import { NoPage } from '../../../component/NoPage'
 import { Markdown } from '../../../component/Markdown'
-import { channel } from 'diagnostics_channel'
 
 export default async (props: Props) => {
   const { md } = props.params
-  const nested = Array.isArray(md)
-  console.log({nested})
-  const path = nested
-    ? md.join('/')
-    : md
-  const currentPath = nested
-    ? md.slice(0, -1).join('/')
-    : undefined
+  const path = md.join('/')
+  const currentPath = md.slice(0, -1).join('/')
 
   try {
-    const markdown = await getData(path)
+    const file = decodeURIComponent(`${DIR_WIKI}/${path}.md`)
+    const markdown = await fs.readFile(file)
+      .then((buffer) => buffer.toString())
       .then(createRelativeLinkReplacer(currentPath))
-      .then(parse)
     const html = marked(markdown)
 
     return (
@@ -48,50 +42,12 @@ type Props = {
 
 export async function generateMetadata({ params }: Props) {
   const path = params.md.join('/')
-  const markdown = await getData(path)
-  const [title, ...lines] = markdown.split('\n')
-  const index = lines.findLastIndex((l) => l.includes('# related'))
-  console.log({index})
-  const keywords = index !== -1
-    ? lines
-      .slice(index + 1)
-      .map((l) => {
-        return RE_RELATION.exec(l)?.[1]
-      })
-      .filter(Boolean)
-      .slice(0, 12)
-    : []
-  const description = lines.join('\n').slice(0, 150)
-  console.log({
-    path,
-        title,
-        description,
-        keywords,
-        openGraph: {
-          title,
-          description,
-          locale: 'ko',
-          siteName: 'https://deptno.dev',
-          url: 'https://deptno.dev/wiki/' + path,
-        },
-      })
 
   return {
-    title,
-    description,
-    keywords,
     openGraph: {
-      title,
-      description,
       locale: 'ko',
       siteName: 'https://deptno.dev',
       url: 'https://deptno.dev/wiki/' + path,
     },
   }
 }
-
-const getData = cache(async (path: string) => {
-  const file = decodeURIComponent(`${DIR_WIKI}/${path}.md`)
-
-  return fs.readFile(file).then((buffer) => buffer.toString())
-})
