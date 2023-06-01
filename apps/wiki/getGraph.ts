7a14dd159c1d87a3097679929735fa5124f896e5
deptno <deptno@gmail.com>
2023-06-02 01:57:45 +0900

    wiki: getGraph 함수를 무인자 함수로, 상수 직접 참조

--- apps/wiki/getGraph.ts
+++ apps/wiki/getGraph.ts
@@ -3,17 +3,18 @@ import { dirname, join } from 'node:path'
 import { getAllMd } from './lib/getAllMd'
 import { RE_VIMWIKI_LINK } from 'parser-vimwiki/constant'
 import { readFileSync } from 'fs'
+import { DIR_WIKI } from './constant'
 
-export const getGraph = (cwd: string) => {
+export const getGraph = () => {
   if (_cache.current) {
     return _cache.current
   }
 
   const graph = createGraph()
-  const map = getAllMd(cwd)
+  const map = getAllMd(DIR_WIKI)
     .map((filepath) => {
       const markdown = readFileSync(filepath).toString()
-      const source = filepath.replace(`${cwd}/`, '').slice(0, -3)
+      const source = filepath.replace(`${DIR_WIKI}/`, '').slice(0, -3)
       const links = (markdown.match(RE_VIMWIKI_LINK) ?? [])
         .map((l) => l.replace(RE_VIMWIKI_LINK, (match, $1) => {
           return $1
