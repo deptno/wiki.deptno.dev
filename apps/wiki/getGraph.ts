40af7723b3acb22a679df77712dc96e1f52451a3
deptno <deptno@gmail.com>
2023-06-01 23:54:13 +0900

    wiki: 그래프 데이터 생성 및 캐시

---
+++ apps/wiki/getGraph.ts
@@ -0,0 +1,43 @@
+import { createGraph, Graph } from './lib/createGraph'
+import { dirname, join } from 'node:path'
+import { getAllMd } from './lib/getAllMd'
+import { RE_VIMWIKI_LINK } from 'parser-vimwiki/constant'
+import { readFileSync } from 'fs'
+
+export const getGraph = (cwd: string) => {
+  if (_cache.current) {
+    return _cache.current
+  }
+
+  const graph = createGraph()
+  const map = getAllMd(cwd)
+    .map((filepath) => {
+      const markdown = readFileSync(filepath).toString()
+      const source = filepath.replace(`${cwd}/`, '').slice(0, -3)
+      const links = (markdown.match(RE_VIMWIKI_LINK) ?? [])
+        .map((l) => l.replace(RE_VIMWIKI_LINK, (match, $1) => {
+          return $1
+            .split('|')[0]
+            .split('#')[0]
+        }))
+        .map((l) => join(dirname(source), l))
+
+      return {
+        source,
+        links,
+      }
+    })
+
+  for (const {source, links} of map) {
+    graph.addNode(source)
+    for (const target of links) {
+      graph.addEdge({ source, target })
+    }
+  }
+
+  return _cache.current = graph
+}
+
+const _cache = {
+  current: undefined as Graph<string>,
+}
