78cf79581aa4dd27236f76e2417e22d75a6329d9
deptno <deptno@gmail.com>
2023-10-02 01:54:39 +0900

    add rollout restart

--- apps/wiki/app/webhook/github/POST.ts
+++ apps/wiki/app/webhook/github/POST.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { exec } from 'node:child_process'
 
 export async function POST(request: NextRequest, { params }: Params) {
   console.debug({ file, params })
@@ -7,7 +8,7 @@ export async function POST(request: NextRequest, { params }: Params) {
       console.debug('json', payload)
 
       if (payload.ref === 'refs/heads/main') {
-        restartToUpdate()
+        restart()
       }
     })
 
@@ -21,7 +22,25 @@ type Params = {
   }
 }
 const file = '/webhook/github/POST'
-const restartToUpdate = () => {
-  console.info('restart to update')
-  process.exit(0)
+const restart = () => {
+  console.info('restart deployment')
+
+  const child = exec(`APISERVER=https://kubernetes.default.svc \
+SERVICEACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount \
+NAMESPACE=\$(cat \${SERVICEACCOUNT}/namespace) \
+TOKEN=\$(cat \${SERVICEACCOUNT}/token) \
+CACERT=\${SERVICEACCOUNT}/ca.crt \
+DEPLOYMENT=deptno-dev \
+; curl -s --cacert \${CACERT} -X PATCH -H "Authorization: Bearer \${TOKEN}" -H "Content-Type: application/strategic-merge-patch+json" --data "{\\"spec\\":{\\"template\\":{\\"metadata\\":{\\"annotations\\":{\\"kubectl.kubernetes.io/restartedAt\\":\\"\\$(date '+%Y-%m-%dT%H:%M:%S%:z')\\"}}}}}" \${APISERVER}/apis/apps/v1/namespaces/\${NAMESPACE}/deployments/\${DEPLOYMENT}`, (error, stdout, stderr) => {
+      if (error) {
+        return console.error(`command error: ${error}`);
+      }
+      console.info(`command stdout: ${stdout}`);
+      if (stderr) {
+        console.error(`command stderr: ${stderr}`);
+      }
+    })
+  child.on('exit', (code) => {
+    console.info(`command exit code ${code}`);
+  })
 }
