c4027e4f4b7f0f97462d3269464e97cef19d9fa8
deptno <deptno@gmail.com>
2023-10-01 10:29:40 +0900

    restart when receive push(main branch) event

--- apps/wiki/app/webhook/github/POST.ts
+++ apps/wiki/app/webhook/github/POST.ts
@@ -2,8 +2,14 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest, { params }: Params) {
   console.debug({ file, params })
-  console.debug('method', request.method)
-  console.debug('json', await request.json())
+  request.json()
+    .then((payload) => {
+      console.debug('json', payload)
+
+      if (payload.ref === 'refs/heads/main') {
+        restartToUpdate()
+      }
+    })
 
   return NextResponse.json({ message: 'ok' }, { status: 200 })
 }
@@ -14,7 +20,8 @@ type Params = {
     method: string
   }
 }
-type Body = {
-  content: string
-}
 const file = '/webhook/github/POST'
+const restartToUpdate = () => {
+  console.info('restart to update')
+  process.exit(0)
+}
