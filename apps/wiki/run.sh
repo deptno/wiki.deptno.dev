464311939dd8668b14681cc1195a2bc3aaff002a
deptno <deptno@gmail.com>
2023-10-15 19:07:58 +0900

    불필요한 파일제거

--- apps/wiki/run.sh
+++
@@ -1,5 +0,0 @@
-URL_WIKI=https://github.com/deptno/public-wiki \
-NEXT_PUBLIC_MEILISEARCH_HOST=https://search.deptno.dev \
-NEXT_PUBLIC_ENDPOINT=http://localhost:3001 \
-DIR_WIKI=/Users/deptno/workspace/src/github.com/deptno/public-wiki \
-pnpm dev
