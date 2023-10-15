89f51a282fe1a34181b6b64364e6523e49685bd8
deptno <deptno@gmail.com>
2023-10-15 19:08:32 +0900

    constant 로 환경변수 모으고 branch 관련 변수 추가

--- apps/meilisearch-updater/src/constant.mts
+++ apps/meilisearch-updater/src/constant.mts
@@ -1,1 +1,3 @@
 export const INDEX = 'wiki'
+export const DIR_WIKI = process.env.DIR_WIKI
+export const NEXT_PUBLIC_MEILISEARCH_HOST = process.env.NEXT_PUBLIC_MEILISEARCH_HOST
--- apps/wiki/constant.ts
+++ apps/wiki/constant.ts
@@ -1,6 +1,7 @@
 export const DIR_WIKI = process.env.DIR_WIKI
+export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT
+export const GIT_BRANCH = process.env.NEXT_PUBLIC_GIT_BRANCH ?? 'main'
 export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 export const MS_CLARITY_ID = process.env.NEXT_PUBLIC_MS_CLARITY_ID
+export const NEXT_PUBLIC_MEILISEARCH_HOST = process.env.NEXT_PUBLIC_MEILISEARCH_HOST
 export const URL_WIKI = process.env.URL_WIKI
-export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT
-export const GIT_BRANCH = process.env.GIT_BRANCH ?? 'main'
