94c766ab18875db7c9d9d08933f9963a5bc87730
deptno <deptno@gmail.com>
2023-10-25 00:07:31 +0900

    support markdown link style []()
    vimwiki 링크만 지원했었음, [], () 안에는 동일한 이름만 허용, 내부 링크기 때문

--- apps/wiki/getGraph.ts
+++ apps/wiki/getGraph.ts
@@ -1,9 +1,10 @@
 import { createGraph, Graph } from './lib/createGraph'
 import { dirname, join } from 'node:path'
 import { getAllMd } from './lib/getAllMd'
-import { RE_VIMWIKI_LINK } from 'parser-vimwiki/constant'
+import { RE_MARKDOWN_LINK, RE_VIMWIKI_LINK } from 'parser-vimwiki/constant'
 import { readFileSync } from 'fs'
 import { DIR_WIKI } from './constant'
+import { getNameFromLink } from './lib/getNameFromLink'
 
 export const getGraph = () => {
   if (_cache.current) {
@@ -12,16 +13,19 @@ export const getGraph = () => {
 
   const graph = createGraph()
   const map = getAllMd(DIR_WIKI)
-    .map((filepath) => {
+    .map((filepath: string) => {
       const markdown = readFileSync(filepath).toString()
       const source = filepath.replace(`${DIR_WIKI}/`, '').slice(0, -3)
-      const links = (markdown.match(RE_VIMWIKI_LINK) ?? [])
-        .map((l) => l.replace(RE_VIMWIKI_LINK, (match, $1) => {
-          return $1
-            .split('|')[0]
-            .split('#')[0]
-        }))
+      const vimwikiLinks = (markdown.match(RE_VIMWIKI_LINK) ?? [])
+        .map((l: string) => l.replace(RE_VIMWIKI_LINK, getNameFromLink))
         .map((l) => join(dirname(source), l))
+      const markdownLinks = (markdown.match(RE_MARKDOWN_LINK) ?? [])
+        .map((l: string) => l.replace(RE_MARKDOWN_LINK, getNameFromLink))
+        .map((l) => join(dirname(source), l))
+      const links = [
+        ...vimwikiLinks,
+        ...markdownLinks,
+      ]
 
       return {
         source,
---
+++ apps/wiki/lib/getNameFromLink.ts
@@ -0,0 +1,7 @@
+// String.prototype.replace 의 콜백으로 사용
+//
+export const getNameFromLink = (_match: string, $1: string) => {
+  return $1
+    .split('|')[0]
+    .split('#')[0]
+}
