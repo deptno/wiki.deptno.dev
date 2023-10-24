a0d66a1cce7309701d4807855812450fcfb0ffb6
deptno <deptno@gmail.com>
2023-10-25 00:54:24 +0900

    build와, 로컬 실행을 위한 dev 파일 추가

---
+++ _dev.sh
@@ -0,0 +1,5 @@
+NEXT_PUBLIC_MEILISEARCH_HOST=localhost:7700 \
+NEXT_PUBLIC_ENDPOINT=https://deptno.dev \
+URL_WIKI=https://github.com/deptno/public-wiki \
+DIR_WIKI=~/workspace/src/github.com/deptno/public-wiki \
+pnpm dev
