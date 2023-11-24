30ac47b8aee462ab713e6ee201c3f5210068ec76
deptno <deptno@gmail.com>
2023-11-24 22:19:38 +0900

    me 페이지 링크를 소문자로 안내

--- apps/wiki/component/MeHeaderLink.tsx
+++ apps/wiki/component/MeHeaderLink.tsx
@@ -3,7 +3,7 @@ import { URL_ME } from "../constant"
 
 export const MeHeaderLink: FC<Props> = () => {
   if (URL_ME) {
-    return <a href={URL_ME}>ME?</a>
+    return <a href={URL_ME}>me?</a>
   }
   return null
 }
