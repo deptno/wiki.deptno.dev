f309f036802488dbfa048310d8da4c4b39679be5
deptno <deptno@gmail.com>
2023-11-24 15:13:27 +0900

    feat: mermaid 렌더링 + pnpm 8
    mermaid 인스톨이 안되서 pnpm 8 로 업그레이드

--- apps/wiki/component/Markdown.tsx
+++ apps/wiki/component/Markdown.tsx
@@ -1,10 +1,12 @@
 import React, { FC, ReactNode } from 'react'
+import { MermaidRenderer } from './MermaidRenderer'
 
 export const Markdown: FC<Props> = (props) => {
   const { data, children } = props
 
   return (
     <div className="w-full overflow-x-hidden">
+      <MermaidRenderer />
       {children}
       <pre
         className="bg-gray-100 markdown p-2 whitespace-pre-wrap"
---
+++ apps/wiki/component/MermaidRenderer.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { useEffect } from 'react'
+import mermaid from 'mermaid'
+
+export const MermaidRenderer = () => {
+  useEffect(() => {
+  const selector = '.language-mermaid'
+  if (document.querySelector(selector)) {
+    mermaid.run({
+      querySelector: '.language-mermaid'
+    })
+
+    }
+  }, [])
+
+  return null
+}
