e723508b2909370e8260f84f42dbf059edfc7d1a
deptno <deptno@gmail.com>
2023-12-05 14:55:03 +0900

    eslint_d 를 가지고 적용해봄 vim 설정과 함께
    + https://github.com/deptno/nvim/commit/84e75694df44e68be036e4bf9a82b30bda60a563

--- apps/wiki/component/MermaidRenderer.tsx
+++ apps/wiki/component/MermaidRenderer.tsx
@@ -4,12 +4,11 @@ import mermaid from 'mermaid'
 
 export const MermaidRenderer = () => {
   useEffect(() => {
-  const selector = '.language-mermaid'
-  if (document.querySelector(selector)) {
-    mermaid.run({
-      querySelector: '.language-mermaid'
-    })
-
+    const selector = '.language-mermaid'
+    if (document.querySelector(selector)) {
+      mermaid.run({
+        querySelector: '.language-mermaid',
+      })
     }
   }, [])
 
