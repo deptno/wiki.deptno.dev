cf8498f18f02d9165d634fd5f05f34ee5fa962a0
deptno <deptno@gmail.com>
2023-12-17 18:13:48 +0900

    support mailto:

--- apps/wiki/lib/marked/link.ts
+++ apps/wiki/lib/marked/link.ts
@@ -7,6 +7,8 @@ export function link(href: string, title: string, text: string) {
     const { protocol } = url
     const protocolName = protocol.slice(0, -1)
 
+    console.log(protocol )
+
     if (protocol) {
       // @ref :h vimwiki-syntax-links
       if (protocol == 'diary:') {
@@ -32,6 +34,12 @@ export function link(href: string, title: string, text: string) {
       ) {
         return `<strike>[${protocolName}] ${text}</strike>`
       }
+      if (protocol === 'mailto:') {
+        const email = href.slice(protocol.length)
+
+        return `<a href="${href}" target="_blank">${email}</a> <a href="https://mail.google.com/mail/?view=cm&fs=1&to=${email}&body=from deptno.dev" target="_blank">[지메일로 보내기]</a>`
+      }
+
       return `<a href="/wiki/${href}">${text}</a>`
     }
   } catch (err) {
