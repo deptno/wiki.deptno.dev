8f7f9a38efe81c07c126f25c3fc3b812c0db2828
deptno <deptno@gmail.com>
2023-12-18 20:41:57 +0900

    wip: [wiki]/index 동작 및 캐시

--- apps/wiki/app/[wiki]/page.tsx
+++ apps/wiki/app/[wiki]/page.tsx
@@ -1,28 +1,26 @@
-import React from 'react'
+import React, { cache } from 'react'
 import fs from 'node:fs/promises'
 import { parse } from 'parser-vimwiki'
-import { DIR_WIKI } from '../../constant'
+import { DIR_WIKI, IS_PROD } from '../../constant'
 import { marked } from '../../lib/marked'
 import { Markdown } from '../../component/Markdown'
 import { Header } from '../../component/Header'
 import { ChildrenWithSearchResult } from '../../component/ChildrenWithSearchResult'
 import { getAllList } from '../../lib/getAllList'
+import { isPublicWiki } from '../../lib/isPublicWiki'
 
-export const dynamic = 'force-dynamic'
 export default async (props: Props) => {
+  const { wiki } = props.params
   const { getRandomLatestModifiedFileName } = getAllList()
 
   try {
-    const markdown = await fs.readFile(`${DIR_WIKI}/index.md`)
-      .then((buffer) => buffer.toString())
-      .then(parse)
-    const html = marked(markdown)
+    const html = await getData(wiki)
 
     return (
       <>
-        <Header placeholder={getRandomLatestModifiedFileName()}/>
-        <ChildrenWithSearchResult/>
-        <Markdown data={html}/>
+        <Header placeholder={getRandomLatestModifiedFileName()} />
+        <ChildrenWithSearchResult />
+        <Markdown data={html} />
       </>
     )
   } catch (err) {
@@ -33,6 +31,19 @@ export default async (props: Props) => {
 
 type Props = {
   params: {
-    md: string
+    wiki: string
   }
 }
+
+async function _getData(wiki: string) {
+  if (!isPublicWiki(wiki)) {
+    return wiki
+  }
+
+  const markdown = await fs
+    .readFile(`${DIR_WIKI}/${wiki}/index.md`)
+    .then(buffer => buffer.toString())
+    .then(parse)
+  return marked(markdown)
+}
+const getData = IS_PROD ? cache(_getData) : _getData
