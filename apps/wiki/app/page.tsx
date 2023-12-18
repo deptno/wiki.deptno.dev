09d6bd7d462126fb1f5c313bb6c9a5c76abd2756
deptno <deptno@gmail.com>
2023-12-18 17:03:46 +0900

    wiki: 루트 페이지에서 위키링크 정상 동작하도록 수정

--- apps/wiki/app/page.tsx
+++ apps/wiki/app/page.tsx
@@ -4,18 +4,23 @@ import { Header } from '../component/Header'
 import { Markdown } from '../component/Markdown'
 import { ChildrenWithSearchResult } from '../component/ChildrenWithSearchResult'
 import { getAllList } from '../lib/getAllList'
+import { CONFIG } from '../constant'
 
 export const dynamic = 'force-dynamic'
 export default async (props: Props) => {
   const { markdowns, lastModified, getRandomLatestModifiedFileName } = getAllList()
-
+  const markdownWiki = CONFIG
+    .map((w) => {
+      return `- [${w.name}](${w.dir})`
+    })
+    .join('\n')
   try {
     return (
       <>
         <Header placeholder={getRandomLatestModifiedFileName()}/>
         <ChildrenWithSearchResult/>
         <div className="p-4 text-lg">위키</div>
-        <Markdown data={marked('- [public-wiki](/)')}/>
+        <Markdown data={marked(markdownWiki)}/>
         <div className="p-4 text-lg">최근 수정</div>
         <Markdown data={marked(lastModified)}/>
         <div className="p-4 text-lg">files</div>
