3f0857156b433658825f3aac3f759cd936a16ca0
deptno <deptno@gmail.com>
2023-12-18 21:45:18 +0900

    nopage graph 에서 없는 파일 및 생성 파일명 정상화

--- apps/wiki/component/NoPage.tsx
+++ apps/wiki/component/NoPage.tsx
@@ -7,19 +7,17 @@ import { GoBack } from './GoBack'
 
 // @ts-ignore
 export const NoPage: FC<Props> = async (props) => {
-  console.log('nopage props', props)
-  const { wiki } = props
-  const [ path ] = props.name.split('/')
-  const graph = getGraph(path)
-  const g = graph.getLinkGraphData(path)
+  const [ wiki, ...paths ] = props.name.split('/')
+  const filename = getFilename(paths)
+  const graph = getGraph(wiki)
+  const g = graph.getLinkGraphData(filename)
 
-  // FIXME:
   return (
     <>
       <Header/>
       <div className="w-full flex flex-col items-center justify-center h-screen">
         <span>존재하지 않는 문서입니다.</span>
-        <a className="underline underline-offset-4" href={`${URL_WIKI}/new/${GIT_BRANCH}?filename=${path}.md`} target="_blank">생성하기</a>
+        <a className="underline underline-offset-4" href={`${URL_WIKI}/new/${GIT_BRANCH}?filename=${filename}.md`} target="_blank">생성하기</a>
         <LinkGraph wiki={wiki} graphData={g}/>
         <GoBack />
       </div>
@@ -28,6 +26,17 @@ export const NoPage: FC<Props> = async (props) => {
 }
 
 type Props = {
-  wiki: string
   name: string
+}
+
+function getFilename(paths: string[]) {
+  if (paths.length >= 2) {
+    if (paths[paths.length - 1] === 'index') {
+      return paths
+        .filter((_, i) => i !== paths.length - 1)
+        .join('/')
+    }
+  }
+
+  return paths.join('/')
 }
