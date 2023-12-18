13c915212096ced5ee3720091cec776afd7b6f77
deptno <deptno@gmail.com>
2023-12-19 00:49:18 +0900

    sitemap.xml 이 wiki.config.js 를 반영하여 public 에서만 + diary 를 제외하고 노출

--- apps/wiki/app/sitemap.ts
+++ apps/wiki/app/sitemap.ts
@@ -1,21 +1,27 @@
 import { MetadataRoute } from 'next'
 import { getAllMd } from '../lib/getAllMd'
-import { DIR_DIARY, DIR_WIKI, ENDPOINT } from '../constant'
+import { DIR_WIKI, ENDPOINT } from '../constant'
+import config from '../wiki.config'
+import path from 'node:path'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return getAllMd(DIR_WIKI)
-    .filter((url) => {
-      return !url.startsWith(`${DIR_WIKI}/${DIR_DIARY}`)
-    })
-    .map((url) => {
-      return url
-        .replace(DIR_WIKI, '')
-        .slice(0, -3)
-    })
-    .map((url) => {
-      return {
-        url: `${ENDPOINT}/wiki${url}`,
-        lastModified: new Date(),
-      }
+  const publicW = (config as Wiki[]).filter(w => !w.private)
+
+  return publicW
+    .flatMap(w => {
+      const dir = path.join(DIR_WIKI, w.dir)
+      const diaryDir = path.join(dir, w.diaryDir)
+
+      return getAllMd(dir)
+        .filter(url => !url.startsWith(diaryDir))
+        .map(url => url.replace(DIR_WIKI, '').slice(0, -3))
+        .map(url => {
+          return {
+            url: `${ENDPOINT}${url}`,
+            // TODO: git 으로 부터 수정시간 기입
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+          }
+        })
     })
 }
