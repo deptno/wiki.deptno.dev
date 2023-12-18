46fa9d2fa0e6cd3dfc5dc2bd7f60d869e6342a29
deptno <deptno@gmail.com>
2023-12-18 23:30:26 +0900

    boolean 리턴을 위해 잘못된 처리 수정

--- apps/wiki/lib/isPublicWiki.ts
+++ apps/wiki/lib/isPublicWiki.ts
@@ -3,5 +3,5 @@ import { CONFIG } from '../constant'
 export async function isPublicWiki(wiki: string): boolean {
   return CONFIG.filter(w => !w.private)
     .map(w => w.dir)
-    .includes(wiki)
+    .some(w => w === wiki)
 }
