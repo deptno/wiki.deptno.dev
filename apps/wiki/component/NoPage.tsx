74d9fde8c8f16cde9faaae9bd071f777073beeb5
deptno <deptno@gmail.com>
2023-12-18 20:52:57 +0900

    wip:  없는 문서 생성시 그래프 링크안됨, 파일이름 정상 아님 마킹

--- apps/wiki/component/NoPage.tsx
+++ apps/wiki/component/NoPage.tsx
@@ -7,17 +7,20 @@ import { GoBack } from './GoBack'
 
 // @ts-ignore
 export const NoPage: FC<Props> = async (props) => {
-  const graph = await getGraph()
+  console.log('nopage props', props)
+  const { wiki } = props
   const [ path ] = props.name.split('/')
+  const graph = getGraph(path)
   const g = graph.getLinkGraphData(path)
 
+  // FIXME:
   return (
     <>
       <Header/>
       <div className="w-full flex flex-col items-center justify-center h-screen">
         <span>존재하지 않는 문서입니다.</span>
         <a className="underline underline-offset-4" href={`${URL_WIKI}/new/${GIT_BRANCH}?filename=${path}.md`} target="_blank">생성하기</a>
-        <LinkGraph graphData={g}/>
+        <LinkGraph wiki={wiki} graphData={g}/>
         <GoBack />
       </div>
     </>
@@ -25,5 +28,6 @@ export const NoPage: FC<Props> = async (props) => {
 }
 
 type Props = {
+  wiki: string
   name: string
 }
