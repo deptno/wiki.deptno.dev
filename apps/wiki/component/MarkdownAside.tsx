a529e7f8aa658a75d8497a1b63a0aadd8ab4b5a7
deptno <deptno@gmail.com>
2023-12-18 23:09:55 +0900

    수정, 기록, 추적버튼의 path 를 wiki base로 재조정

--- apps/wiki/component/MarkdownAside.tsx
+++ apps/wiki/component/MarkdownAside.tsx
@@ -10,16 +10,17 @@ import { DiaryNavigation } from './DiaryNavigation'
 export const MarkdownAside: FC<Props> = (props) => {
   const { data, wiki, path = '' } = props
   const graph = getGraph(wiki)
-  const g = graph.getLinkGraphData(path.slice(wiki.length + 1))
+  const wikiBasedPath = path.slice(wiki.length + 1)
+  const g = graph.getLinkGraphData(wikiBasedPath)
   const branch = GIT_BRANCH
 
   return (
     <div className="p-2">
       <ChildrenWithSearchResult>
         <div className="flex justify-end gap-2 border-b-2">
-          <a className="underline border-l-blue-400" href={`${URL_WIKI}/edit/${branch}/${path}.md`} target="_blank">수정</a>
-          <a className="underline border-l-blue-800" href={`${URL_WIKI}/commits/${branch}/${path}.md`} target="_blank">기록</a>
-          <a className="underline border-l-blue-800" href={`${URL_WIKI}/blame/${branch}/${path}.md`} target="_blank">추적</a>
+          <a className="underline border-l-blue-400" href={`${URL_WIKI}/edit/${branch}/${wikiBasedPath}.md`} target="_blank">수정</a>
+          <a className="underline border-l-blue-800" href={`${URL_WIKI}/commits/${branch}/${wikiBasedPath}.md`} target="_blank">기록</a>
+          <a className="underline border-l-blue-800" href={`${URL_WIKI}/blame/${branch}/${wikiBasedPath}.md`} target="_blank">추적</a>
         </div>
         <LinkGraph wiki={wiki} graphData={g}/>
         <hr/>
