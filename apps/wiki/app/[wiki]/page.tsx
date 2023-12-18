1397cb3dafc2fcd48add6b13a89220a1cf71bb27
deptno <deptno@gmail.com>
2023-12-19 00:02:50 +0900

    /wiki 으로 접근시에 /wiki/index 로 리다이렉트

--- apps/wiki/app/[wiki]/page.tsx
+++ apps/wiki/app/[wiki]/page.tsx
@@ -1,32 +1,9 @@
-import React, { cache } from 'react'
-import fs from 'node:fs/promises'
-import { parse } from 'parser-vimwiki'
-import { DIR_WIKI, IS_PROD } from '../../constant'
-import { marked } from '../../lib/marked'
-import { Markdown } from '../../component/Markdown'
-import { Header } from '../../component/Header'
-import { ChildrenWithSearchResult } from '../../component/ChildrenWithSearchResult'
-import { getAllList } from '../../lib/getAllList'
-import { isPublicWiki } from '../../lib/isPublicWiki'
+import { redirect } from "next/navigation"
 
 export default async (props: Props) => {
   const { wiki } = props.params
-  const { getRandomLatestModifiedFileName } = getAllList()
 
-  try {
-    const html = await getData(wiki)
-
-    return (
-      <>
-        <Header placeholder={getRandomLatestModifiedFileName()} />
-        <ChildrenWithSearchResult />
-        <Markdown data={html} />
-      </>
-    )
-  } catch (err) {
-    console.error(err)
-    throw err
-  }
+  return redirect(decodeURIComponent(`/${wiki}/index`))
 }
 
 type Props = {
@@ -34,17 +11,3 @@ type Props = {
     wiki: string
   }
 }
-
-async function _getData(wiki: string) {
-  if (!isPublicWiki(wiki)) {
-    return wiki
-  }
-
-  const markdown = await fs
-    .readFile(`${DIR_WIKI}/${wiki}/index.md`)
-    .then(buffer => buffer.toString())
-    .then(parse)
-
-  return marked(markdown)
-}
-const getData = IS_PROD ? cache(_getData) : _getData
