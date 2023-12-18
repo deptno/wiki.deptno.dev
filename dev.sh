b34c884bd6a101029de8f1beb5f41ecc77f30086
deptno <deptno@gmail.com>
2023-12-18 20:08:34 +0900

    wip: URL_ME path 변경

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 NEXT_PUBLIC_MEILISEARCH_HOST=localhost:7700 \
 NEXT_PUBLIC_ENDPOINT=http://localhost:3001 \
-URL_ME=/wiki/about-me \
+URL_ME=/public-wiki/about-me \
 URL_WIKI=https://github.com/deptno/public-wiki \
 DIR_WIKI=~/workspace/src/github.com/deptno \
 pnpm dev
