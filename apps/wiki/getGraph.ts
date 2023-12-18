0f872056eb35595c6c688738910c6583f06bb866
deptno <deptno@gmail.com>
2023-12-18 22:28:33 +0900

    fmt

--- apps/wiki/getGraph.ts
+++ apps/wiki/getGraph.ts
@@ -16,51 +16,48 @@ export const getGraph = (wiki: string) => {
 
   const graph = createGraph()
   const wikiDir = join(DIR_WIKI, wiki)
-  const handleVimwikiPrefix  = createVimwikiPrefixHandler(CONFIG.find(w => w.dir === wiki)?.diaryDir ?? 'diary')
-  const map = getAllMd(wikiDir)
-    .map((filepath: string) => {
-      const markdown = readFileSync(filepath).toString()
-      const source = filepath.replace(`${wikiDir}/`, '').slice(0, -3)
-      const vimwikiLinks = (markdown.match(RE_VIMWIKI_LINK) ?? [])
-        .map((l: string) => {
-          return join(
-            getVimwikiLinkBaseName(source, l),
-            l.replace(RE_VIMWIKI_LINK, getNameFromLink)
-          )
-        })
-      const markdownLinks = (
-        markdown
-           // vimwiki markdown 설정에서 enter 를 치면 `-` 대신 space가 이름에 입력됨
-          .replace(/ /g, '-')
-          .match(RE_MARKDOWN_LINK)
-        ?? []
+  const handleVimwikiPrefix = createVimwikiPrefixHandler(
+    CONFIG.find(w => w.dir === wiki)?.diaryDir ?? 'diary',
+  )
+  const map = getAllMd(wikiDir).map((filepath: string) => {
+    const markdown = readFileSync(filepath).toString()
+    const source = filepath.replace(`${wikiDir}/`, '').slice(0, -3)
+    const vimwikiLinks = (markdown.match(RE_VIMWIKI_LINK) ?? []).map(
+      (l: string) => {
+        return join(
+          getVimwikiLinkBaseName(source, l),
+          l.replace(RE_VIMWIKI_LINK, getNameFromLink),
+        )
+      },
+    )
+    const markdownLinks = (
+      markdown
+        // vimwiki markdown 설정에서 enter 를 치면 `-` 대신 space가 이름에 입력됨
+        .replace(/ /g, '-')
+        .match(RE_MARKDOWN_LINK) ?? []
+    ).map((l: string) => {
+      return join(
+        getMakrdownLinkBaseName(source, l),
+        l.replace(RE_MARKDOWN_LINK, getNameFromLink),
       )
-        .map((l: string) => {
-          return join(
-            getMakrdownLinkBaseName(source, l),
-            l.replace(RE_MARKDOWN_LINK, getNameFromLink)
-          )
-        })
-      const links = [
-        ...vimwikiLinks,
-        ...markdownLinks,
-      ]
-        .map(handleVimwikiPrefix)
-        .filter(Boolean)
-
-      return {
-        source,
-        links,
-      }
     })
+    const links = [...vimwikiLinks, ...markdownLinks]
+      .map(handleVimwikiPrefix)
+      .filter(Boolean)
 
-  for (const {source, links} of map) {
+    return {
+      source,
+      links,
+    }
+  })
+
+  for (const { source, links } of map) {
     for (const target of links) {
       graph.addEdge({ source, target })
     }
   }
 
-  return _cache.current = graph
+  return (_cache.current = graph)
 }
 
 const _cache = {
