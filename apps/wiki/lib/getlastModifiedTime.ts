d07fa379694efc8bb6b651ca7438f306981d9df8
deptno <deptno@gmail.com>
2025-07-25 23:51:53 +0900

    feat: sitemap modified time 적용

---
+++ apps/wiki/lib/getlastModifiedTime.ts
@@ -0,0 +1,25 @@
+import { execSync } from 'node:child_process'
+
+export function getLastModifiedTime(params: Params): Date {
+  const { dir, pathname } = params
+
+  try {
+    const command = `git -C ${dir} log -1 --format="%cI" -- ${pathname}`
+    const result = execSync(command)
+      .toString()
+      .trim()
+
+    return new Date(result)
+  } catch(err) {
+    console.warn({ err, file, dir, pathname }, 'fail to get log')
+
+    return new Date()
+  }
+}
+
+const file = import.meta.url
+
+type Params = {
+  dir: string
+  pathname: string
+}
