e7aaeefaf82991181c75ca5995cb4829554b097e
deptno <deptno@gmail.com>
2025-07-25 18:33:41 +0900

    fix: 30일내 수정사항이 없는 경우 git 명령어 깨짐

--- apps/wiki/lib/getLastModifiedFiles.ts
+++ apps/wiki/lib/getLastModifiedFiles.ts
@@ -4,8 +4,16 @@ import path from 'node:path'
 
 export function getLastModifiedFiles(wiki: Wiki): string[] {
   const dir = path.join(DIR_WIKI_ROOT, wiki.dir)
+  const hash = execSync(`git -C ${dir} log --since="30 days ago" --pretty=format:"%H" | tail -1`)
+    .toString()
+    .trim()
+
+  if (!hash) {
+    return []
+  }
+
   const command = `
-git -C ${dir} diff $(git -C ${dir} log --since="30 days ago" --pretty=format:"%H" | tail -1).. --stat |
+git -C ${dir} diff ${hash}.. --stat |
 sed '$d' |
 awk -F'|' '{ print $2 $1 }' |
 sort -nr |
