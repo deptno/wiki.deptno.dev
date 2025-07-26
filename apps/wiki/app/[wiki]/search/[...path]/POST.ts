7e0d841626fbce6ace5ded5d01114f08e98c97fa
deptno <deptno@gmail.com>
2025-07-26 22:42:47 +0900

    fix: 프록시 시에 헤더 새로 만들어야함

--- apps/wiki/app/[wiki]/search/[...path]/POST.ts
+++ apps/wiki/app/[wiki]/search/[...path]/POST.ts
@@ -3,26 +3,24 @@ import { NEXT_PUBLIC_MEILISEARCH_API_KEY, NEXT_PUBLIC_MEILISEARCH_HOST } from '.
 
 export async function POST(req: Request, context: Context) {
   const params = await context.params
+  console.debug({ file, params })
   const { path } = params
 
   try {
     const api = `${NEXT_PUBLIC_MEILISEARCH_HOST}/${path.join('/')}`
-    const body = await req.json()
-    const headers = NEXT_PUBLIC_MEILISEARCH_API_KEY
-      ? {
-        ...req.headers,
-        authorization: `Bearer ${NEXT_PUBLIC_MEILISEARCH_API_KEY}`
-      }
-      : req.headers
+    const headers = new Headers(req.headers)
+    if (NEXT_PUBLIC_MEILISEARCH_API_KEY) {
+      headers.set('authorization', `Bearer ${NEXT_PUBLIC_MEILISEARCH_API_KEY}`)
+    }
 
     const response = await fetch(api, {
-      method: 'POST',
       headers,
-      body: JSON.stringify(body),
+      method: 'POST',
+      body: await req.text()
     })
 
     return new Response(response.body, {
-      status: response.status
+      status: response.status,
     })
   } catch (err) {
     console.error({ file, err })
